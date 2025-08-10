c94615c161b9597747c43c0dd705ad3912fa84ba
ivan <ivanlowlh@gmail.com>
2025-08-10 13:41:46 +0800

    Add basic num display

--- script.js
+++ script.js
@@ -26,3 +26,16 @@ function operate(a, b, operator) {
             return divide(a, b);
     }
 }
+
+let display_number = 0;
+
+function displayNum (num_button) {
+    const number = num_button.textContent;
+    const num_display = document.querySelector("#display_num");
+    num_display.textContent = number;
+}
+
+const number_btns = document.querySelectorAll(".num");
+number_btns.forEach((num_btn) => {
+    num_btn.addEventListener("click", () => displayNum(num_btn))
+})
